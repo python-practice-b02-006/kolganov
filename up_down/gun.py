b75f5e5644da329863361c85b3dea59fffc92334
kolganovn <nikita.kolganov@phystech.edu>
2020-10-23 11:24:36 +0300

    Complete ball class, add it's test

--- up_down/gun.py
+++ up_down/gun.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import numpy as np
+from random import randint
 
 SCREEN_SIZE = (800, 600)
 BLACK = (0, 0, 0)
@@ -7,8 +8,44 @@ RED = (255, 0, 0)
 
 pg.init()
 
+
 class Ball():
-    pass
+    def __init__(self, coord, vel, rad=15, color=None):
+        if color == None:
+            color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.color = color
+        self.coord = coord
+        self.vel = vel
+        self.rad = rad
+
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, self.coord, self.rad)
+
+    def move(self, t_step=1.):
+        for i in range(2):
+            self.coord[i] += int(self.vel[i] * t_step)
+        self.check_walls()
+
+    def check_walls(self):
+        n = [[1, 0], [0, 1]]
+        for i in range(2):
+            if self.coord[i] < self.rad:
+                self.coord[i] = self.rad
+                self.flip_vel(n[i])
+            elif self.coord[i] > SCREEN_SIZE[i] - self.rad:
+                self.coord[i] = SCREEN_SIZE[i] - self.rad
+                self.flip_vel(n[i])
+
+    def flip_vel(self, axis, coef_perp=1., coef_par=1.):
+        vel = np.array(self.vel)
+        n = np.array(axis)
+        n = n / np.linalg.norm(n)
+        vel_perp = vel.dot(n) * n
+        vel_par = vel - vel_perp
+        ans = -vel_perp * coef_perp + vel_par * coef_par
+        print(vel, ans)
+        self.vel = ans.astype(np.int).tolist()
+
 
 class Table():
     pass
@@ -37,15 +74,24 @@ class Manager():
     def __init__(self):
         self.gun = Gun()
         self.table = Table()
+        self.balls = []
+        self.balls.append(Ball([100, 100], [10, 20]))
     
     def process(self, events, screen):
         done = self.handle_events(events)
+        self.move()
         self.draw(screen)
         return done
 
     def draw(self, screen):
         screen.fill(BLACK)
+        for ball in self.balls:
+            ball.draw(screen)
         self.gun.draw(screen)
+
+    def move(self):
+        for ball in self.balls:
+            ball.move()
     
     def handle_events(self, events):
         done = False
