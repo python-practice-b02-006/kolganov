6d903996f64f24308cd19a8314405538941eea0a
kolganovn <nikita.kolganov@phystech.edu>
2020-10-27 18:53:30 +0300

    Add short socstrings

--- gun/gun.py
+++ gun/gun.py
@@ -65,7 +65,13 @@ class Ball():
         pg.draw.circle(screen, self.color, self.coord, self.rad)
         
 class Target():
+    '''
+    Target class. Creates target, manages it's rendering and collision with a ball event.
+    '''
     def __init__(self, coord=None, color=None, rad=30):
+        '''
+        Constructor method. Sets coordinate, color and radius of the target.
+        '''
         if coord == None:
             coord = [randint(rad, SCREEN_SIZE[0] - rad), randint(rad, SCREEN_SIZE[1] - rad)]
         self.coord = coord
@@ -76,16 +82,28 @@ class Target():
         self.color = color
 
     def check_collision(self, ball):
+        '''
+        Checks whether the ball bumps into target.
+        '''
         dist = sum([(self.coord[i] - ball.coord[i])**2 for i in range(2)])**0.5
         min_dist = self.rad + ball.rad
         return dist <= min_dist
 
     def draw(self, screen):
+        '''
+        Draws the target on the screen
+        '''
         pg.draw.circle(screen, self.color, self.coord, self.rad)
 
 
 class Gun():
+    '''
+    Gun class. Manages it's renderring, movement and striking.
+    '''
     def __init__(self, coord=[30, SCREEN_SIZE[1]//2], angle=0, max_pow=50, min_pow=10, color=RED):
+        '''
+        Constructor method. Sets coordinate, direction, minimum and maximum power and color of the gun.
+        '''
         self.coord = coord
         self.angle = angle
         self.max_pow = max_pow
@@ -95,13 +113,22 @@ class Gun():
         self.pow = min_pow
     
     def activate(self):
+        '''
+        Activates gun's charge.
+        '''
         self.active = True
 
     def gain(self, inc=2):
+        '''
+        Increases current gun charge power.
+        '''
         if self.active and self.pow < self.max_pow:
             self.pow += inc
 
     def strike(self):
+        '''
+        Creates ball, according to gun's direction and current charge power.
+        '''
         vel = self.pow
         angle = self.angle
         ball = Ball(list(self.coord), [int(vel*np.cos(angle)), int(vel*np.sin(angle))])
@@ -110,13 +137,22 @@ class Gun():
         return ball
         
     def set_angle(self, target_pos):
+        '''
+        Sets gun's direction to target position.
+        '''
         self.angle = np.arctan2(target_pos[1] - self.coord[1], target_pos[0] - self.coord[0])
 
     def move(self, inc):
+        '''
+        Changes vertical position of the gun.
+        '''
         if (self.coord[1] > 30 or inc > 0) and (self.coord[1] < SCREEN_SIZE[1] - 30 or inc < 0):
             self.coord[1] += inc
 
     def draw(self, screen):
+        '''
+        Draws the gun on the screen.
+        '''
         gun_shape = []
         vec_1 = np.array([int(5*np.cos(self.angle - np.pi/2)), int(5*np.sin(self.angle - np.pi/2))])
         vec_2 = np.array([int(self.pow*np.cos(self.angle)), int(self.pow*np.sin(self.angle))])
@@ -129,12 +165,18 @@ class Gun():
 
 
 class ScoreTable():
+    '''
+    Score table class.
+    '''
     def __init__(self, t_destr=0, b_used=0):
         self.t_destr = t_destr
         self.b_used = b_used
         self.font = pg.font.SysFont("dejavusansmono", 25)
 
     def score(self):
+        '''
+        Score calculation method.
+        '''
         return self.t_destr - self.b_used
 
     def draw(self, screen):
@@ -147,6 +189,9 @@ class ScoreTable():
 
 
 class Manager():
+    '''
+    Class that manages events' handling, ball's motion and collision, target creation, etc.
+    '''
     def __init__(self, n_targets=1):
         self.balls = []
         self.gun = Gun()
@@ -156,11 +201,17 @@ class Manager():
         self.new_mission()
 
     def new_mission(self):
+        '''
+        Adds new targets.
+        '''
         for i in range(self.n_targets):
             self.targets.append(Target(rad=randint(max(1, 30 - 2*max(0, self.score_t.score())), 
                 30 - max(0, self.score_t.score()))))
 
     def process(self, events, screen):
+        '''
+        Runs all necessary method for each iteration. Adds new targets, if previous are destroyed.
+        '''
         done = self.handle_events(events)
 
         if pg.mouse.get_focused():
@@ -177,6 +228,9 @@ class Manager():
         return done
 
     def handle_events(self, events):
+        '''
+        Handles events from keyboard, mouse, etc.
+        '''
         done = False
         for event in events:
             if event.type == pg.QUIT:
@@ -196,6 +250,9 @@ class Manager():
         return done
 
     def draw(self, screen):
+        '''
+        Runs balls', gun's, targets' and score table's drawing method.
+        '''
         for ball in self.balls:
             ball.draw(screen)
         for target in self.targets:
@@ -204,6 +261,9 @@ class Manager():
         self.score_t.draw(screen)
 
     def move(self):
+        '''
+        Runs balls' and gun's movement method, removes dead balls.
+        '''
         dead_balls = []
         for i, ball in enumerate(self.balls):
             ball.move(grav=2)
@@ -214,6 +274,9 @@ class Manager():
         self.gun.gain()
 
     def collide(self):
+        '''
+        Checks whether balls bump into targets, sets balls' alive trigger.
+        '''
         collisions = []
         targets_c = []
         for i, ball in enumerate(self.balls):
