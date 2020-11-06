fab56fc42c72e4c615aa5afad182c7839c12a9f3
kolganovn <nikita.kolganov@phystech.edu>
2020-11-06 09:58:00 +0300

    Remove testing files

--- branches/features.py
+++
@@ -1,9 +0,0 @@
-class MyFeatures():
-    def __init__(self):
-        pass
-
-    def feature_1(self):
-        print("Feature 1 is done")
-
-    def feature_2(self):
-        print("Feature 2 is done")
