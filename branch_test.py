a4aa39ec64805f2f1f72032452b8c0d06673ec2c
kolganovn <nikita.kolganov@phystech.edu>
2020-11-06 13:55:46 +0300

    remive branches

--- branch_test.py
+++
@@ -1,3 +0,0 @@
-class FeatureClass():
-    def __init__(self):
-        pass
--- branches/features.py
+++
@@ -1,6 +0,0 @@
-class FeatureClass():
-    def __init__(self):
-        print("New class constructor!")
-
-    def feature(self):
-        print("New nice feature!")
